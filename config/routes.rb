b7b042cb60616ef7cd71c6e183ce79cfd6177843
Adrian082818 <mohabiradrian@gmail.com>
2020-03-21 15:14:26 -0400

    will add routing to models and create views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'application#home'
+  # devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
 
-  # devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
+  devise_scope :user do 
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+  end 
+
+  resources :exercises, only: [:new, :create, :index, :show, :update, :destroy, :edit]
+
+  resources :coaches do 
+    resources :exercises, only: [:index]
+  end 
+
 end
