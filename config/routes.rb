9f1c60640c2f39304c6466d01157e1b24999791f
Adrian082818 <mohabiradrian@gmail.com>
2020-03-31 22:13:43 -0400

    will fix table alignment in index show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#home'
-  # devise_for :users
-  # devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
-
-  # devise_scope :user do 
-  #   get 'login', to: 'devise/sessions#new'
-  #   get 'signup', to: 'devise/registrations#new'
-  # end 
 
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
   
@@ -17,11 +10,11 @@ Rails.application.routes.draw do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
   end
-  
+  resources :users 
   resources :exercises, only: [:new, :create, :index, :show, :update, :destroy, :edit]
 
   resources :coaches do 
-    resources :exercises, only: [:index]
+    resources :exercises, only: [:index, :show, :create]
   end 
 
 end
